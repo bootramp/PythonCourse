1bd186b1544de45f97039f97b55f4febca141d3e
Milad Mahmoodian <127476479+bootramp@users.noreply.github.com>
2024-01-30 07:19:09 +0330

    Update main.py

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -10,15 +10,36 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 1
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
+def reset_timer():
+    global reps
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    status_text.config(text="Timer")
+    check_symbol.config(text="")
+    reps = 1
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
 def start_timer():
-    count_down(5 * 60)
-
+    global reps
+    work_min_in_sec = WORK_MIN * 60
+    short_break_in_sec = SHORT_BREAK_MIN * 60
+    long_break_in_sec = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        status_text.config(text="Break", fg=RED)
+        count_down(long_break_in_sec)
+    elif reps % 2 == 0:
+        status_text.config(text="Break", fg=PINK)
+        count_down(short_break_in_sec)
+    elif reps % 2 == 1:
+        status_text.config(text="Work", fg=GREEN)
+        count_down(work_min_in_sec)
+    reps += 1
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
@@ -32,8 +53,17 @@ def count_down(count):
         # Python is Dynamic Lan
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-    if count > 0 :
-        window.after(1000, count_down, count - 1)
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        check_mark = math.floor(reps/2)
+        for _ in range(check_mark):
+            mark = "✔"
+        check_symbol.config(text=mark)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -48,23 +78,17 @@ canvas.create_image(111, 120, image=tomato_img)
 timer_text = canvas.create_text(111, 140, text="00:00", fill="white", font=(FONT_NAME, 40, "bold"))
 canvas.grid(column=2, row=1)
 
-status_text = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"), highlightthickness=0)
+status_text = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"), highlightthickness=0)
 status_text.grid(column=2, row=0)
 
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=1, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=3, row=2)
 
-check_symbol = Label(text="✔", fg=GREEN, bg=YELLOW, font=(26), highlightthickness=0)
+check_symbol = Label(fg=GREEN, bg=YELLOW, font=(26), highlightthickness=0)
 check_symbol.grid(column=2, row=3)
-
-
-
-
-
-
 
 
 window.mainloop()
